94964c74f0e60d97c9115ad2a32ce4ae943b1fb7
Elkin Romero <gio.busrom@gmail.com>
2020-01-30 19:17:03 -0700

    video 7 finished

--- js/scriptures.js
+++ js/scriptures.js
@@ -26,6 +26,7 @@ const Scriptures=(function(){
        const BOTTOM_PADDING="<br /><br />";
        const CLASS_BOOKS="books";
        const CLASS_BUTTON="btn";
+       const CLASS_CHAPTER="chapter";
        const CLASS_VOLUME="volumes";
        const DIV_SCRIPTURES_NAVIGATOR="scripnav";
        const DIV_SCRIPTURES="scriptures";
@@ -51,6 +52,8 @@ const Scriptures=(function(){
         let booksGrid;
         let booksGridContent;
         let cacheBooks;
+        let chaptersContent;
+        let chaptersGridContent;
         let htmlAnchor;
         let htmlDiv;
         let htmlElement;
@@ -138,6 +141,34 @@ const Scriptures=(function(){
                 callback();
             }
         };
+        chaptersContent = function (book){
+            return htmlDiv({
+                classKey:CLASS_VOLUME,
+                content: htmlElement(TAG_VOLUME_HEADER, book.fullName)
+            }) + htmlDiv({
+                classKey: CLASS_BOOKS,
+                content: chaptersGridContent(book)
+            });
+        };
+
+        chaptersGridContent = function(book){
+          let gridContent= "";
+          let chapter = 1;
+          while (chapter < book.numChapters) {
+              gridContent += htmlLink({
+                classKey: `${CLASS_BUTTON} ${CLASS_CHAPTER}`,
+                id: chapter,
+                href: `#0:${book.id}:${chapter}`,
+                content:chapter
+              });
+
+              chapter += 1;
+          }
+
+          return gridContent;
+        };
+
+
         htmlAnchor = function (volume) {
             return `<a name="v${volume.id}" />`;
         };
@@ -227,7 +258,16 @@ const Scriptures=(function(){
 
         };
         navigateBook = function(bookId){
-          console.log("navigateBook"+ bookId);
+            let book =books[bookId];
+            if(book.numChapters <= 1){
+              navigateChapter(book.id, book.numChapters);
+            } else{
+                document.getElementById(DIV_SCRIPTURES).innerHTML = htmlDiv({
+                  id:DIV_SCRIPTURES_NAVIGATOR,
+                  content: chaptersContent(book)
+                });
+            }
+
         };
         navigateChapter = function(bookId, chapter){
           console.log("navigateChapter"+ bookId +", "+ chapter);
